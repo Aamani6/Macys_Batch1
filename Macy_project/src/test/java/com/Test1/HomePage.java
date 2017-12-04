ddf9201a850f7ba25a276764e7eda4031d2d79ca
12 <Lakshmi@DESKTOP-46TS987>
2017-12-03 22:04:11 -0600

    Check in code to GitHub

---
+++ Macy_project/src/test/java/com/Test1/HomePage.java
@@ -0,0 +1,16 @@
+package com.Test1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.macys.com/");
+		
+
+	}
+
+}
